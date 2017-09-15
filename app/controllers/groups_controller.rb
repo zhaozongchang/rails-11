544f58592736f457f3208371f7862c4b80483a67
zhaozongchang <xingyingsuifeng@gmail.com>
2017-09-15 18:29:53 +0800

    use before_acion to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -24,39 +25,30 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert:"你没有权限！"
-    end
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert:"你没有权限！"
-    end
-
     if @group.update(group_params)
-    redirect_to groups_path, notice:"更新成功！"
-  else
-    render :edit
-   end
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
+
+  private
+
+  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
     if current_user != @group.user
       redirect_to root_path, alert: "你没有权限！"
     end
-    
-    @group.destroy
-    redirect_to groups_path , alert:"删除成功！"
   end
-
-  private
 
   def group_params
     params.require(:group).permit(:title, :description)
